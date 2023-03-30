36e818da7433eafc8be0731f2c645cf40486f4af
muneebx786 <129435966+muneebx786@users.noreply.github.com>
2023-03-31 00:24:12 +0500

    Add files via upload
    ok

---
+++ app.js
@@ -0,0 +1,61 @@
+alert("How are you");
+
+console.log("javscript");
+
+
+var title ="javascript";
+console.log( title);
+
+
+var a = 10;
+var b = 20; 
+var c = 50;
+var x =(a + b) - c
+console.log(x);
+
+
+var x ="Muneeb";
+var y ="Khan";
+
+
+var z = x + y;
+console.log(z)
+
+
+var a =20;
+a++;
+console.log(a)
+
+
+var b =20;
+b++;
+console.log(b)
+
+
+
+var a =20;
+a--;
+console.log(a)
+
+
+var b =20;
+b--;
+console.log(b)
+
+
+var firstname = prompt("enter your name");
+var lastname = prompt("enter your lastname");
+var city = prompt("enter city", "Karachi");
+console.log(firstname + " " + lastname + " From " + city);
+
+
+var maiaarahaho = true;
+
+if (maiaarahaho == true) {
+    console.log("good Morning");
+} else{
+    console.log("Good Night");
+}
+
+
+
